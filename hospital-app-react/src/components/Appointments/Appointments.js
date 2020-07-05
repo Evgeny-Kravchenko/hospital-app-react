592ed73a9f78da688a3dd2db159a0ddcbf59541b
Evgeny Kravchenko <evgen.kravchenko.vladimirovich@mail.ru>
2020-07-05 12:21:40 +0300

    feat: add the filter form

--- hospital-app-react/src/components/Appointments/Appointments.js
+++ hospital-app-react/src/components/Appointments/Appointments.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
 
+import { Form } from "reactstrap";
 import Moment from "react-moment";
-
-import "./Appointments.scss";
+import { filter } from "underscore";
 
 import Table from "../Table/Table";
 import Header from "../Header/Header";
+import TextField from "../TextField/TextField";
+import DateField from "../DateField/DateField";
+import CheckboxField from "../CheckboxField/CheckboxField";
+
+import "./Appointments.scss";
 
 import { ReactComponent as Appointment } from "../../images/medical-appointment.svg";
 
@@ -13,8 +18,50 @@ import { appointments as data } from "../../lib/MockData";
 
 const TITLE = "Приёмы";
 
+const USER = "Иванов Иван Иванович";
+
 export default class Appointments extends Component {
+  state = {
+    filter: {
+      startDate: null,
+      endDate: null,
+      clientName: "",
+      onlyMe: false,
+    },
+  };
+
+  onChangeFilterField = (name, value) => {
+    const { filter } = this.state;
+
+    this.setState({
+      filter: { ...filter, ...{ [name]: value } },
+    });
+  };
+
+  onChangeFilterDateField = (name, value) => {
+    const { filter } = this.state;
+
+    this.setState({
+      filter: { ...filter, ...{ [name]: value && value.getTime() } },
+    });
+  };
+
   render() {
+    const { startDate, endDate, clientName, onlyMe } = this.state.filter;
+
+    let filtered = filter(data, (o) => {
+      return (
+        (startDate ? o.date >= startDate : true) &&
+        (endDate ? o.date <= endDate : true) &&
+        (clientName
+          ? clientName.length > 2
+            ? o.clientName.includes(clientName)
+            : true
+          : true) &&
+        (onlyMe ? o.holderName === USER : true)
+      );
+    });
+
     return (
       <div className="Appointments">
         <Header
@@ -23,41 +70,94 @@ export default class Appointments extends Component {
           className="Appointments-Header"
           renderIcon={() => <Appointment className="Header-Icon" />}
         />
-        <div className="Appointments-Body">
+        <div className='Appointments-Body'>
+          <div className='Appointments-Filter'>
+            <Form className='Appointments-FilterForm'>
+              <DateField
+                  hasTime
+                  name='startDate'
+                  value={startDate}
+                  dateFormat='dd/MM/yyyy HH:mm'
+                  timeFormat='HH:mm'
+                  placeholder='С'
+                  className='Appointments-FilterField'
+                  onChange={this.onChangeFilterDateField}
+              />
+              <DateField
+                  hasTime
+                  name='endDate'
+                  value={endDate}
+                  dateFormat='dd/MM/yyyy HH:mm'
+                  timeFormat='HH:mm'
+                  placeholder='По'
+                  className='Appointments-FilterField'
+                  onChange={this.onChangeFilterDateField}
+              />
+              <TextField
+                  name='clientName'
+                  value={clientName}
+                  placeholder='Клиент'
+                  className='Appointments-FilterField'
+                  onChange={this.onChangeFilterField}
+              />
+              <CheckboxField
+                  name='onlyMe'
+                  label='Только я'
+                  value={onlyMe}
+                  className='Appointments-FilterField'
+                  onChange={this.onChangeFilterField}
+              />
+            </Form>
+          </div>
           <Table
-            data={data}
-            columns={[
-              {
-                dataField: "date",
-                text: "Дата",
-                headerStyle: {
-                  width: "200px",
+              data={filtered}
+              className='AppointmentList'
+              columns={[
+                {
+                  dataField: 'date',
+                  text: 'Дата',
+                  headerStyle: {
+                    width: '150px'
+                  },
+                  formatter: (v) => {
+                    return (
+                        <Moment date={v} format='DD.MM.YYYY HH.mm'/>
+                    )
+                  }
                 },
-                formatter: (v) => {
-                  return <Moment date={v} format="DD.MM.YYYY HH.mm" />;
+                {
+                  dataField: 'clientName',
+                  text: 'Клиент',
+                  headerStyle: {
+                    width: '300px'
+                  }
                 },
-              },
-              {
-                dataField: "clientName",
-                text: "Клиент",
-              },
-              {
-                dataField: "status",
-                text: "Статус",
-              },
-              {
-                dataField: "holderName",
-                text: "Принимающий",
-              },
-              {
-                dataField: "compliences",
-                text: "Жалобы",
-              },
-              {
-                dataField: "diagnosis",
-                text: "Диагноз",
-              },
-            ]}
+                {
+                  dataField: 'status',
+                  text: 'Статус'
+                },
+                {
+                  dataField: 'holderName',
+                  text: 'Принимающий',
+                  headerStyle: {
+                    width: '300px'
+                  }
+                },
+                {
+                  dataField: 'compliences',
+                  text: 'Жалобы',
+                  headerStyle: {
+                    width: '200px'
+                  }
+                },
+                {
+                  dataField: 'diagnosis',
+                  text: 'Диагноз',
+                  headerStyle: {
+                    width: '200px'
+                  }
+                }
+              ]}
           />
         </div>
       </div>
